33b20bab448e04f4cca59992c1d1fc0cac1cd84a
laannn2914000 <lanhoang29012004@gmail.com>
2025-03-09 23:09:10 +0700

    new theme

--- local-setup.cmd
+++
@@ -1,5 +0,0 @@
-cd C:\laragon\www
-git clone https://github.com/FitHanuSpecialSubject/fsa-wordpress.git .
-curl https://raw.githubusercontent.com/suyttthideptrai/database_backup/refs/heads/main/_wordpress_12-03.sql --output C:\laragon\db-backup.sql
-cd C:\laragon\bin\mysql\mysql-8.0.30-winx64\bin
-mysql -u root < C:\laragon\db-backup.sql
